8775b8f19b5e993a8f82635f1efd1e620f3f0855
brentru <robots199@me.com>
2019-05-10 11:55:14 -0400

    start mocking it out

--- adafruit_azureiot.py
+++ adafruit_azureiot.py
@@ -44,3 +44,47 @@ Implementation Notes
 
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_AzureIoT.git"
+
+AZURE_API_VERSION = "2018-06-30" # Azure URI API Version Identifier
+
+class iot_hub:
+    """
+    Provides access to Azure IoT Hub.
+    https://docs.microsoft.com/en-us/rest/api/iothub/
+    """
+    def __init__(wifi_manager, iot_hub_name, sas_token):
+        """ Creates an instance of an Azure IoT Hub Client.
+        :param wifi_manager: WiFiManager object from ESPSPI_WiFiManager
+        :param str iot_hub_name: Name of your IoT Hub
+        :param str sas_token: SAS Token Identifier
+                                (https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-devguide-security)
+        """
+        wifi_type = str(type(wifi_manager))
+        if 'ESPSPI_WiFiManager' in wifi_type:
+            self.wifi = wifi_manager
+        else:
+            raise TypeError("This library requires a WiFiManager object.")
+        self._iot_hub_url = "https://{0}.azure-devices.net".format(iot_hub_name)
+        self._sas_token = sas_token
+
+    # HTTP Request Methods
+    def _post(self, path, payload):
+        """POSTs data to an Azure IoT Hub
+        :param str path: Formatted URL
+        :param json payload: JSON Data
+        """
+        response = self.wifi.post(
+            path,
+            json=payload,
+            headers=self._create_headers(self._aio_headers[0]))
+        self._handle_error(response)
+        return response.json()
+
+
+    # Device Messaging 
+    # D2C: Device-to-Cloud
+    # C2D: Cloud-to-Device
+
+    # IoT Hub Service
+
+    # IoT Hub Resource Provider
